1f97fa7562bd9a28f2a77ff9436821918d5aacb6
Yazeed Saad <yazotaibi@gmail.com>
2019-09-24 11:23:34 +0300

    refactoring the code

--- js/app.js
+++ js/app.js
@@ -1,26 +1,19 @@
-const deck = document.querySelector('.deck');
-/*
- * Create a list that holds all of your cards
- */
-const allCards = document.getElementById('deck').getElementsByClassName("card");
- // Display the cards on the page
 const movesCounter = document.querySelector(".score-panel").querySelector('.moves');
-let moves = 0;
+let openCards = [];
 matchedCards = 0;
- const allCardsAr = Array.from(allCards);
- let openCards = [];
-//  shuffle the list of cards using the provided "shuffle" method below
-const shuffledCards = shuffle(allCardsAr);
+let moves = 0;
 
-/*
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-deck.innerHTML = "";
-for ( const card of shuffledCards){
-    deck.appendChild(card);
+function newGame(){
+    const deck = document.querySelector('.deck');
+    const allCards = document.getElementById('deck').getElementsByClassName("card");
+    const allCardsAr = Array.from(allCards);
+    const shuffledCards = shuffle(allCardsAr);
+    deck.innerHTML = "";
+    for ( const card of shuffledCards){
+        deck.appendChild(card);
+    }
+    addListener()
 }
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -36,16 +29,38 @@ function shuffle(array) {
     return array;
 }
 
+function addListener() {
+    deck.addEventListener("click", showCard);
+}
+function removeListener(){
+    deck.removeEventListener("click", showCard);
+}
+
 function showCard(e) {
-    
+    removeListener()
     if (e.target.className === "card") {
         e.target.classList.add("show", "open");
         openCards.push(e.target);
 
     }if (openCards.length === 2){
         checkCards()
+    }else {
+        addListener();
     }
  }
+
+ function checkCards(){
+    moves++;
+    movesCounter.innerHTML = moves;
+    removeListener()
+    openCards[1].classList.add("show");
+    if(openCards[0].innerHTML === openCards[1].innerHTML) {
+        setTimeout(cardsMatch, 1000);
+    }else if(openCards[0] != openCards[1].innerHTML) {
+        setTimeout(notMatch, 1000);
+        addListener()
+    }
+}
 
 function notMatch() {
     const nomatch = deck.querySelectorAll(".open");
@@ -55,18 +70,8 @@ function notMatch() {
     }
     openCards = [];
 }
-function checkCards(){
-    moves++;
-    movesCounter.innerHTML = moves;
-    removeListener()
-    openCards[1].classList.add("show");
-    if(openCards[0].innerHTML === openCards[1].innerHTML) {
-        setTimeout(cardsMatch, 1000);
-    }else if(openCards[0] != openCards[1].innerHTML) {
-        setTimeout(notMatch, 1000);
-        addEvent()
-    }
-}
+
+
 function cardsMatch(){
     const matched = deck.querySelectorAll(".open");
     for (const i of matched) {
@@ -74,23 +79,10 @@ function cardsMatch(){
         i.classList.remove('show');
         i.classList.add("match")
     };
+    matchedCards++
+    console.log(matchedCards);
     openCards = [];
-    addEvent()
+    addListener()
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-function addEvent() {
-    deck.addEventListener("click", showCard);
-}
-function removeListener(){
-    deck.removeEventListener("click", showCard);
-}
-addEvent()
+
+newGame()
